2e0891c3843e0b868ab4cd40e45b8a7eb5a657de
SukruYuksel <yksl3743dc@gmail.com>
2024-01-04 17:35:16 +0100

    Test1 ve Test2 eklendi

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Nach dem Javalernen fange ich neu Testatomatiom");
+        System.out.println("Test Automation ");
+
+    }
+}
